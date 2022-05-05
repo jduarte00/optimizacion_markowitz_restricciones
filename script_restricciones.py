a6b7e6a543fe3def29266bb78bafa0b6f57da057
Jose Duarte <dumejo92@gmail.com>
2022-05-05 15:07:15 -0500

    last commit temp after reset

--- script_restricciones.py
+++ script_restricciones.py
@@ -7,19 +7,14 @@ from cvxopt import solvers
 from cvxopt.blas import dot
 solvers.options['show_progress'] = False
 # ---------------------------
-# CARGA DE DATOS
+# CARGA DE DATOS (Especificar aquí la fuente del csv donde están los retornos de los activos)
 retpor = pd.read_csv('./retpor.csv', index_col=  'Date')
-print(retpor.head(4))
 # ---------------------------
 # CÁLCULO DE PARÁMETROS
 E = np.cov(retpor.T)
 g = np.mean(retpor, axis = 0)
-print(np.shape(E))
-print(np.shape(g))
 E_matrix = matrix(E)
 g_matrix = matrix(g)
-print(E_matrix)
-print(g_matrix)
 n = len(g)
 N = 100
 phis = [10**(5.0*t/N-1.0) for t in range(N)]
@@ -32,21 +27,27 @@ phis = [10**(5.0*t/N-1.0) for t in range(N)]
 # riesgo_ganancia_minima = [np.sqrt(portfolio.T @ E @ portfolio ) for portfolio in portfolios_ganancia_minima]
 # rest_ganancia_minima = pd.DataFrame(data = {"riesgo":riesgo_ganancia_minima,"retorno": retornos_ganancia_minima})
 # # ---------------------------
-# # RESTRICCION MINIMA VARIANZA
-# A = matrix(1.0, (1,n))
-# b = matrix(1.0)
-# portfolios = [ qp(P = phi*E_matrix, q = -g_matrix,A= A, b =b)['x'] for phi in phis ]
-# returns = [ dot(g_matrix,x) for x in portfolios ]
-# risks = [ np.sqrt(dot(x, E_matrix*x)) for x in portfolios ]
-# risks_df = np.array(risks)
-# returns_df = np.array(returns)
-# rest_todo_capital = pd.DataFrame(data = {"riesgo":risks_df,"retorno": returns_df, "phi": phis})
-# print(rest_todo_capital.head(10))
+# # RESTRICCION DE ALOCACIÓN DE TODOS LOS ACTIVOS
 
+## RESTRICCIÓN DE SUM(x_i) = 1 SOBRE TODO I
+A = matrix(1.0, (1,n))
+b = matrix(1.0)
+
+portfolios = [ qp(phi*E_matrix, -g_matrix, A, b)['x'] for phi in phis ]
+returns = [ dot(g_matrix,x) for x in portfolios ]
+risks = [ np.sqrt(dot(x, E_matrix*x)) for x in portfolios ]
+risks_df = np.array(risks)
+returns_df = np.array(returns)
+rest_todos_activos= pd.DataFrame(data = {"riesgo":risks_df,"retorno": returns_df, "phi": phis})
+rest_todos_activos.to_csv('./markowitz_todo_capital.csv')
+print(np.min(risks_df))
+print(np.max(risks_df))
+print(np.min(returns_df))
+print(np.max(returns_df))
 # --------------------
 # RESTRICCION DE NO VENTAS EN CORTO + ASIGNACIÓN DE TODO EL CAPIAL + NO APALANCAMIENTO 
 
-# RESTRICCIÓN DE x_i >= 0 para todo 9
+# RESTRICCIÓN DE x_i >= 0 para todo i
 G = matrix(0.0, (n,n))
 G[::n+1] = -1.0
 h = matrix(0.0, (n,1))
@@ -55,14 +56,15 @@ h = matrix(0.0, (n,1))
 A = matrix(1.0, (1,n))
 b = matrix(1.0)
 
-# Definición de número de valores de phi que se van a calcular.
 portfolios = [ qp(phi*E_matrix, -g_matrix, G, h, A, b)['x'] for phi in phis ]
 returns = [ dot(g_matrix,x) for x in portfolios ]
 risks = [ np.sqrt(dot(x, E_matrix*x)) for x in portfolios ]
 risks_df = np.array(risks)
 returns_df = np.array(returns)
+print(np.min(risks_df))
+print(np.max(risks_df))
+print(np.min(returns_df))
+print(np.max(returns_df))
 answers_dataframe = pd.DataFrame(data = {"riesgo":risks_df,"retorno": returns_df, "phi": phis})
-print(answers_dataframe.head(10))
-answers_dataframe.to_csv('./markowitz.csv')
-print(np.array(portfolios[-1]))
-print(np.array(portfolios[-1]).sum())
+answers_dataframe.to_csv('./markowitz_nocorto_noapal_todo_capital.csv')
+
